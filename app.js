916eae341315d0d3818792e02c6f4cc879f8e2d9
eopong87 <eopong87@gmail.com>
2023-07-27 10:44:08 -0400

    javaScript done

--- app.js
+++ app.js
@@ -4,7 +4,35 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "John";
+let lastName = "Williams";
+var age = 78;
+console.log(age);
 
 // Exercise 2
+lastName = "williams";
+age = 34;
 
+console.log(lastName)
 // Exercise 3
+let language = "JavaScript";
+let CreatedYear =1995;
+let isCaseSensitive = true;
+console.log(language)
+
+//Exercise 4
+let price = 19.99;
+let inOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(stock)
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = "722";
+let bookmark = "456";
+let hasRead = "true";
+console.log(bookmark);
